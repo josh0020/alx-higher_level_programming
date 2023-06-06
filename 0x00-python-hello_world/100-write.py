c046de40c02a0c9c36253f12d50440da7aba493e
josh <joshuaking34111573@gmail.com>
2023-06-06 04:39:12 -0700

    fix

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,4 @@
-from sys import stderr
-stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19')
-sys. exit(1)
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
