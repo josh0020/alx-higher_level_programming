aff3c827df94ebe808ae0439d56b5af2d90b4cac
josh <joshuaking34111573@gmail.com>
2023-09-28 13:51:59 -0700

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,23 +3,32 @@
 
 
 def find_peak(list_of_integers):
-    if list_of_integers == []:
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): The list of unsorted integers.
+
+    Returns:
+        int or None: The peak integer found in the list or None if the list is empty.
+
+    Note:
+        There may be more than one peak in the list.
+
+    Complexity:
+        This algorithm has a time complexity of O(log(n)).
+    """
+    if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
-    mid = int(length / 2)
-    li = list_of_integers
+    left, right = 0, len(list_of_integers) - 1
 
-    if mid - 1 < 0 and mid + 1 >= length:
-        return li[mid]
-    elif mid - 1 < 0:
-        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= length:
-        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+    while left < right:
+        mid = (left + right) // 2
 
-    if li[mid - 1] < li[mid] > li[mid + 1]:
-        return li[mid]
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
 
-    if li[mid + 1] > li[mid - 1]:
-        return find_peak(li[mid:])
-    return find_peak(li[:mid])
+    return list_of_integers[left]
